b02159f20766eccb6d7c9e86397628a4440fa39b
nthigadenis <denno@DESKTOP-TK85V7G.localdomain>
2022-10-11 22:34:25 +0300

    useeffects

--- src/components/Bookscontainer.js
+++ src/components/Bookscontainer.js
@@ -3,3 +3,20 @@ import BookForm from './BookForm'
 import BookList from './BookList'
 import {Switch, Route} from 'react-router-dom'
 function BooksContainer() {
+
+    const[Books,setBooks]=useState([]);
+    const [selectedCategory, setSelectedCategory]=useState("")
+    const [searchQuery, setSearchQuery]=useState("");
+  
+    const onAddBook =(newBook)=>{
+        setBooks(Books => [...Books,
+         newBook])
+     
+       }
+         const onDeleteBook= (deletedBookId)=>{
+         setBooks(Books=>Books.filter(Book=>{
+           return Book.id !== deletedBookId
+         }))
+         }
+
+
