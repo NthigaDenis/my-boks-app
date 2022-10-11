63813cf69e9629c664a19e3c37c9c108c41f0755
nthigadenis <denno@DESKTOP-TK85V7G.localdomain>
2022-10-11 22:43:52 +0300

    Done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,16 @@
 
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+
+
+const Home = () => {
+    const [recentBooks, setRecentBooks] = useState([]);
+  
+    useEffect(() => {
+      // fetch the 3 most recently added projects from json-server
+      fetch("https://rehtse-library.herokuapp.com/books?_sort=id&_order=desc&_limit=3")
+        .then((r) => r.json())
+        .then((recentBooks) => {
+          setRecentBooks(recentBooks);
+        });
+    }, []);
