3726def9ae2feebe345a40ce73c2728b43432d9d
nthigadenis <denno@DESKTOP-TK85V7G.localdomain>
2022-10-11 22:38:15 +0300

    still updating

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,3 +4,22 @@ import {NavLink, Link} from 'react-router-dom'
   
 const handleToggleDarkModeClick=(e)=>{
   onToggleDarkMode();
+
+  
+}
+return (
+  <header>
+  <h1 className='branding'>
+     <Link to ="/">
+       Home
+       </Link>
+      </h1>
+      <nav>
+    <div className='navigation'>
+
+      <NavLink className='button' to ="/books">
+        Books
+      </NavLink>
+      <NavLink  className='button' to ="/books/new">
+        Add Books
+      </NavLink>
